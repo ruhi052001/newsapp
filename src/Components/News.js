513b7ca7fca74f873b551ed3b3f665105378b422
ruhi052001 <b20307@students.iitmandi.ac.in>
2023-02-11 14:58:00 +0530

    updated newsapp

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,11 +2,76 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  articles=[
+    {
+        "source": {
+            "id": "news-com-au",
+            "name": "News.com.au"
+        },
+        "author": null,
+        "title": "Test in major strife after damning findings",
+        "description": "<p>The third Test between India and Australia is at risk of being moved to a different venue at the eleventh hour, with Dharamsala&rsquo;s Himachal Pradesh Cricket Association Stadium reportedly not fit to host international cricket.</p>",
+        "url": "https://www.news.com.au/sport/cricket/dharamsala-at-risk-of-losing-bordergavaskar-trophy-test-as-bcci-ponders-eleventhhour-venue-change/news-story/454b80f794a0be03885c090bbb36dc6f",
+        "urlToImage": "https://content.api.news/v3/images/bin/e90d0ebb2d685e2c25015b6e6ca530c2",
+        "publishedAt": "2023-02-11T04:37:00Z",
+        "content": "The third Test between India and Australia is at risk of being moved to a different venue at the eleventh hour, with Dharamsala’s Himachal Pradesh Cricket Association Stadium reportedly not fit to ho… [+2243 chars]"
+    },
+    {
+        "source": {
+            "id": "espn-cric-info",
+            "name": "ESPN Cric Info"
+        },
+        "author": null,
+        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+        "publishedAt": "2020-04-27T11:41:47Z",
+        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+    },
+    {
+        "source": {
+            "id": "espn-cric-info",
+            "name": "ESPN Cric Info"
+        },
+        "author": null,
+        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+        "publishedAt": "2020-03-30T15:26:05Z",
+        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+    }
+]
+  constructor(){
+    super();
+    console.log("constructor running from News Component!!");
+    this.state={
+        articles : this.articles,
+        loading: false
+    }
+
+  }
+
+  async componentDidMount(){
+    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=fe7bc2235a324c0e97b6e763ab6b6341";
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData);
+    this.setState({articles:parseData.articles})
+  }
   render() {
     return (
-      <div>
-        This is news component!
-        <NewsItem/>
+      <div className='container my-4'>
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+        {this.state.articles.map((element)=>{
+          return <div className="col-md-4" key={element.url}>
+            <NewsItem  title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+          </div>
+        })}
+          
+        </div>
       </div>
     )
   }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+  
   render() {
+    let {title, description,imageUrl,newsUrl} = this.props
     return (
-      <div>
-        This is a news item!
+      <div className='my-3'>
+        <div className="card" style={{width: "18rem"}}>
+            <img src={!imageUrl?"https://www.hindustantimes.com/ht-img/img/2023/02/11/1600x900/Turkey_1676087044843_1676087045074_1676087045074.jpg":imageUrl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+            </div>
+        </div>
       </div>
     )
   }
