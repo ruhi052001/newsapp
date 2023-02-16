f7ed3441fe701f34c7b472911618bfec6a114cff
ruhi052001 <b20307@students.iitmandi.ac.in>
2023-02-16 18:39:21 +0530

    updated changes

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,76 +1,88 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
-  articles=[
-    {
-        "source": {
-            "id": "news-com-au",
-            "name": "News.com.au"
-        },
-        "author": null,
-        "title": "Test in major strife after damning findings",
-        "description": "<p>The third Test between India and Australia is at risk of being moved to a different venue at the eleventh hour, with Dharamsala&rsquo;s Himachal Pradesh Cricket Association Stadium reportedly not fit to host international cricket.</p>",
-        "url": "https://www.news.com.au/sport/cricket/dharamsala-at-risk-of-losing-bordergavaskar-trophy-test-as-bcci-ponders-eleventhhour-venue-change/news-story/454b80f794a0be03885c090bbb36dc6f",
-        "urlToImage": "https://content.api.news/v3/images/bin/e90d0ebb2d685e2c25015b6e6ca530c2",
-        "publishedAt": "2023-02-11T04:37:00Z",
-        "content": "The third Test between India and Australia is at risk of being moved to a different venue at the eleventh hour, with Dharamsala’s Himachal Pradesh Cricket Association Stadium reportedly not fit to ho… [+2243 chars]"
-    },
-    {
-        "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-        "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-]
+  static defaultProps = {
+    country : 'in',
+    pageSize : 6,
+    category : 'general'
+  }
+
+  static propTypes = {
+    country : PropTypes.string,
+    pageSize : PropTypes.number,
+    category : PropTypes.string,
+  }
   constructor(){
     super();
     console.log("constructor running from News Component!!");
     this.state={
-        articles : this.articles,
-        loading: false
+        articles : [],
+        loading: false,
+        page: 1
     }
 
   }
 
   async componentDidMount(){
-    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=fe7bc2235a324c0e97b6e763ab6b6341";
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe7bc2235a324c0e97b6e763ab6b6341&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
     let data = await fetch(url);
     let parseData = await data.json();
     console.log(parseData);
-    this.setState({articles:parseData.articles})
+    this.setState({articles:parseData.articles,
+      totalResults:parseData.totalResults,
+    loading: false})
   }
+
+  handlePrevClick = async () => {
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe7bc2235a324c0e97b6e763ab6b6341&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData);
+    
+    this.setState({
+      page: this.state.page - 1,
+      articles: parseData.articles,
+      loading: false
+    })
+  }
+
+  handleNextClick = async () => {
+    if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
+      let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe7bc2235a324c0e97b6e763ab6b6341&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true});
+      let data = await fetch(url);
+      let parseData = await data.json();
+      
+      this.setState({
+        page: this.state.page + 1,
+        articles: parseData.articles,
+        loading: false
+      })
+    }
+  }
+
   render() {
     return (
       <div className='container my-4'>
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1 className="text-center" style={{margin: '35px 0px'}}>NewsMonkey - Top Headlines</h1>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-        {this.state.articles.map((element)=>{
+        {!this.state.loading && this.state.articles.map((element)=>{
           return <div className="col-md-4" key={element.url}>
-            <NewsItem  title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+            <NewsItem  title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
           </div>
         })}
           
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-  
+
   render() {
-    let {title, description,imageUrl,newsUrl} = this.props
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props
     return (
       <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
-            <img src={!imageUrl?"https://www.hindustantimes.com/ht-img/img/2023/02/11/1600x900/Turkey_1676087044843_1676087045074_1676087045074.jpg":imageUrl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}</p>
-              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
-            </div>
+        <div className="card">
+          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex:'1' }}>
+            {source}
+          </span>
+          <img src={!imageUrl ? "https://www.hindustantimes.com/ht-img/img/2023/02/11/1600x900/Turkey_1676087044843_1676087045074_1676087045074.jpg" : imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small class="text-muted">By {!author ? "unkonwn" : author} on {new Date(date).toGMTString()} </small></p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+          </div>
         </div>
       </div>
     )
